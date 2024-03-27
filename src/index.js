744f0d01ff68cf16f912a0012a53eb1686842da6
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-03-27 11:28:44 +0530

    added my solution for profile card v2

--- src/index.js
+++ src/index.js
@@ -6,55 +6,82 @@ const person = {
   fullName: "Jonas Schmedtmann",
   photoSrc: "/images/profile.jpg",
   bio: `Hi, I'm working as a manual tester with 2.5 years of experience. I looking for a role in front-end development. I like to play PC games, cook and drive my motor cycle.`,
-  skills: [
-    "Java 👌",
-    "Python 😊",
-    "HTML 😊",
-    "CSS 😊",
-    "JavaScript 😊",
-    "React 😒",
-  ],
 };
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#FF00FF",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+];
 
 function App() {
   return (
     <div className="profile-card">
-      <ProfilePhoto src={person.photoSrc} fullName={person.fullName} />
-      <ProfileName fullName={person.fullName} bio={person.bio} />
-      <ProfileSkills skills={person.skills} />
+      <ProfilePhoto person={person} />
+      <ProfileName person={person} />
+      <ProfileSkills skills={skills} />
     </div>
   );
 }
 
-function ProfilePhoto(props) {
-  return <img src={props.src} alt={props.fullName}></img>;
+function ProfilePhoto({ person }) {
+  return <img src={person.src} alt={person.fullName}></img>;
 }
-function ProfileName(props) {
+function ProfileName({ person }) {
   return (
     <div className="data">
-      <h3>{props.fullName}</h3>
-      <p>{props.bio}</p>
+      <h3>{person.fullName}</h3>
+      <p>{person.bio}</p>
     </div>
   );
 }
 
-function ProfileSkills(props) {
+function ProfileSkills({ skills }) {
   return (
     <div className="skill-list">
-      <Skill skill={props.skills[0]} bgColor="rgb(254, 47, 47)" />
-      <Skill skill={props.skills[1]} bgColor="rgb(59, 255, 59)" />
-      <Skill skill={props.skills[2]} bgColor="rgb(50, 50, 255)" />
-      <Skill skill={props.skills[3]} bgColor="rgb(255, 24, 243)" />
-      <Skill skill={props.skills[4]} bgColor="rgb(255, 255, 62)" />
-      <Skill skill={props.skills[5]} bgColor="rgb(48, 255, 255)" />
+      {skills.map((skill) => (
+        <Skill skill={skill} />
+      ))}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ skill }) {
   return (
-    <div className="skill-item" style={{ backgroundColor: props.bgColor }}>
-      {props.skill}
+    <div className="skill-item" style={{ backgroundColor: skill.color }}>
+      {skill.skill}{" "}
+      {skill.level === "beginner"
+        ? "👶"
+        : skill.level === "intermediate"
+        ? "👍"
+        : "💪"}
     </div>
   );
 }
